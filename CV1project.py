673b5410ea79fe0f5595a64685a19a46a950e37e
flyingrockingit <coolavniroy@gmail.com>
2026-08-29 16:31:13 +0300

    Add files via upload

---
+++ CV1project.py
@@ -0,0 +1,27 @@
+import cv2
+
+image = cv2.imread("closeup-shot-beautiful-butterfly-with-interesting-textures-orange-petaled-flower.jpg")
+
+#Resize the image into three different sizes
+
+small_image = cv2.resize(image,(200,200))
+
+medium_image = cv2.resize(image,(400,400))
+
+large_image = cv2.resize(image,(600,600))
+
+cv2.imshow("Small Image - 200x200",small_image)
+
+cv2.imshow("Medium Image - 400x400",small_image)
+
+cv2.imshow("Large Image - 600x600",large_image)
+
+cv2.imwrite("input_image_small.jpg", small_image)
+
+cv2.imwrite("input_image_medium.jpg", medium_image)
+
+cv2.imwrite("input_image_large.jpg", large_image)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
